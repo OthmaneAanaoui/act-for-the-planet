81558c9f7b59f55a7cee31aa22a3818ebdb0bd1b
william fundaro <wfundaro@gmail.com>
2021-03-17 11:53:58 +0100

    update code pour test

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 #include "header/IntervalManager.h"
 
 UltraSonic sonic1 = UltraSonic();
-UltraSonic sonic2 = UltraSonic();
-UltraSonic sonic3 = UltraSonic();
+// UltraSonic sonic2 = UltraSonic();
+// UltraSonic sonic3 = UltraSonic();
 
 Presence detectPresence = Presence();
 
 InfraRed wasteDetector1 = InfraRed();
-InfraRed wasteDetector2 = InfraRed();
-InfraRed wasteDetector3 = InfraRed();
+// InfraRed wasteDetector2 = InfraRed();
+// InfraRed wasteDetector3 = InfraRed();
 
 Communication com = Communication();
 IntervalManager timer = IntervalManager();
@@ -23,29 +23,40 @@ void setup()
   /* Initialise le port série */
   com.init(9600);
   sonic1.init(13, 12);
-  sonic2.init(11,10);
-  sonic3.init(9,8);
+  // sonic2.init(11,10);
+  // sonic3.init(9,8);
 
   detectPresence.init(1);
 
   wasteDetector1.init(2);
-  wasteDetector2.init(3);
-  wasteDetector3.init(4);
+  // wasteDetector2.init(3);
+  // wasteDetector3.init(4);
 
 }
 
 void loop()
 {
-  Serial.print(F("Distance détecteur 1 : "));
-  Serial.println(sonic1.getDistance());
+  // Serial.print(F("Distance détecteur 1 : "));
+  // Serial.println(sonic1.getDistance());
 
-  Serial.print(F("Distance détecteur 2 : "));
-  Serial.println(sonic2.getDistance());
+  // Serial.print(F("Distance détecteur 2 : "));
+  // Serial.println(sonic2.getDistance());
 
-  if(detectPresence.getIsDetect() == PresenceState::startDetecting) {
-    Serial.println("Motion detected");
+  // if(detectPresence.getIsDetect() == PresenceState::startDetecting) {
+  //   Serial.println("Motion detected");
+  // }
+  //com.readSerialPort();
+
+  if(wasteDetector1.getIsDetect() == PresenceState::endDetecting) {
+    delay(1000);
+    com.sendData((String)sonic1.getDistance());
   }
-
+  
+  //pour test à virer pour l'appli finale
+  if(detectPresence.getIsDetect() == PresenceState::endDetecting) {
+    delay(1000);
+    com.sendData((String)sonic1.getDistance());
+  }
 
   /* Délai d'attente pour éviter d'afficher trop de résultats à la seconde */
   delay(timer.getInterval());
